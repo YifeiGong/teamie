0797447cc1faae79e4af44325449011871f0258b
Shreyas Tallamraju <shreyastallamraju2020@u.northwestern.edu>
2019-10-29 17:07:49 -0500

    updated voting page

--- src/voting.js
+++ src/voting.js
@@ -1,59 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Card from '@material-ui/core/Card';
-import Component from 'react';
 import Poll from 'react-polls';
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAFzpavaaS5qMRo8FSZsqsZAaglgXL8H04",
-    authDomain: "teamie-blue.firebaseapp.com",
-    databaseURL: "https://teamie-blue.firebaseio.com",
-    projectId: "teamie-blue",
-    storageBucket: "teamie-blue.appspot.com",
-    messagingSenderId: "373175945503",
-    appId: "1:373175945503:web:0ce516f07c5d387642882a"
-  };
-  
-  
-  const db = firebase.database().ref();
+//create the database
+const db = firebase.database();
 
 // Declaring poll question and answers
 const pollQuestion = 'Select Restaurant';
-let pollAnswers = [
-  {option: '', votes: 0},
-]
-
+let pollState = []
 
 const VotingApp = ({match}) => {
     const eventID = match.params.id;
     const [currRestaurants, setCurrRestaurants] = useState({restaurants: []});
-    const [pollResponses, setPollResponses] = useState({pollResponses: pollAnswers});
 
     useEffect(() => {
         const handleData = snap => {
           if (snap.val()) setCurrRestaurants(snap.val());
         }
-        db.on('value', handleData, error => alert(error));
+        
+        db.ref().on('value', handleData, error => alert(error));
         return () => {
-          db.off('value', handleData);
+          db.ref().off('value', handleData);
         };
       }, []);
-
-    const handleVote = () => {
-        /* 
-        
-        This is where we update firebase data?
-
-        */
-        
-        console.log("voted!");
-
-
+    
+    const handleVote = (e) => {
+      const idx = restaurantPollList.indexOf(e);
+      let newVotingData = currRestaurants.votingData[eventID];
+      newVotingData[idx] += 1;
+      db.ref("votingData/"+eventID+"/").set(
+        newVotingData
+      );
     }
 
     let restaurantPollList = eventID.split(",").map((v) => {
@@ -64,25 +42,36 @@ const VotingApp = ({match}) => {
             }
         }
     });
+
     restaurantPollList = restaurantPollList.filter((v) => v !== undefined);
-    console.log(restaurantPollList);
+
+    useEffect(() => {
+      try {
+        const temp = currRestaurants.votingData[eventID];
+      }
+      catch (error) {
+        //create entry    
+        //db.ref("votingData/"+eventID+"/").set(
+          //new Array(restaurantPollList.length).fill(0)
+        //);
+      }
+    },[currRestaurants]); // run after currRestaurants has been loaded
+
+    pollState = [];
 
     for(var y = 0; y < restaurantPollList.length; y++) {
         const tempDict = {
             option: restaurantPollList[y],
-            votes: 0 
+            votes: currRestaurants.votingData[eventID][y],
         };
-        console.log(tempDict)
-        pollAnswers.push(tempDict);
+        pollState.push(tempDict);
     }
-
-    console.log(pollAnswers);
 
     return(
 
         // <h1>Voting for Event</h1>
         <Poll question={pollQuestion} 
-        answers={pollAnswers} 
+        answers={pollState} 
         onVote={handleVote}
         />
 
